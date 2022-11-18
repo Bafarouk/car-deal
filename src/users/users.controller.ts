209ed4ebdb1b1c2ace12f07a7a4a5b44cb496940
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2022-11-18 17:29:47 +0100

    add authentication Guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -18,6 +19,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { currentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -33,6 +35,7 @@ export class UsersController {
   // }
 
   @Get('/currentuser')
+  @UseGuards(AuthGuard)
   currentUser(@currentUser() user: User) {
     return user;
   }
@@ -54,6 +57,7 @@ export class UsersController {
   }
 
   @Post('/signout')
+  @UseGuards(AuthGuard)
   signOut(@Session() session: any) {
     session.userId = null;
   }
