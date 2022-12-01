45667018af43a9a85aaacdd04fbba8bf1abf5d70
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2022-12-01 13:30:11 +0100

    change CurrentUser interceptor to middleware

--- src/reports/controllers/reports.controller.ts
+++ src/reports/controllers/reports.controller.ts
@@ -14,6 +14,7 @@ import { User } from 'src/users/entity/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptors';
 import { ReportDto } from '../dtos/report.dto';
 import { ApproveReportDto } from '../dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -27,6 +28,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: number, @Body() body: ApproveReportDto) {
     return this.reportsService.approve(id, body.approved);
   }
