658c058267c293dcde2b1fb24aea691a8c6245f2
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2022-12-02 12:18:12 +0100

    get estimated report createQueryBuilder

--- src/reports/services/reports.service.ts
+++ src/reports/services/reports.service.ts
@@ -4,6 +4,7 @@ import { Report } from '../entity/report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDTO } from '../dtos/create-report.dto';
 import { User } from 'src/users/entity/user.entity';
+import { GetEstimateDto } from '../dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -25,5 +26,21 @@ export class ReportsService {
 
     report.approved = approved;
     return this.reportRepository.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, mileage, year }: GetEstimateDto) {
+    return this.reportRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
